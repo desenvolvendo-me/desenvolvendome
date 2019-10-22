7e4e1272ca6e0b9a3c6e4fa20287f1338e3e0344
Marco Castro <marcodotcastro@gmail.com>
2019-10-22 16:05:46 -0300

    [r] portal do cidadão para desenvolvendo me

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "portal_cidadao_production"
+  # config.active_job.queue_name_prefix = "desenvolvendo_me_production"
 
   config.action_mailer.perform_caching = false
 
