f1131481a7e91f6e5ed3afc8e9e37b0d9bab0334
Marco Castro <marcodotcastro@gmail.com>
2019-10-23 09:54:34 -0300

    [c] avaliar perfil

--- app/controllers/perfis_controller.rb
+++ app/controllers/perfis_controller.rb
@@ -1,14 +1,28 @@
 class PerfisController < ApplicationController
+
   def new
     @perfil = Perfil.new
   end
 
   def show
+    @perfil = Perfil.find(params[:id])
+  end
+
+  def create
+    @perfil = Perfil.new(perfil_params)
+
+    respond_to do |format|
+      if @perfil.save
+        format.html {redirect_to @perfil}
+      else
+        format.html {render :new}
+      end
+    end
   end
 
   private
 
-  def contato_params
+  def perfil_params
     params.require(:perfil).permit(:github)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 # == Route Map
 #
 #                                Prefix Verb   URI Pattern                                                                              Controller#Action
-#                           reclamacoes GET    /reclamacoes(.:format)                                                                   reclamacoes#index
-#                                       POST   /reclamacoes(.:format)                                                                   reclamacoes#create
-#                        new_reclamacao GET    /reclamacoes/new(.:format)                                                               reclamacoes#new
-#                       edit_reclamacao GET    /reclamacoes/:id/edit(.:format)                                                          reclamacoes#edit
-#                            reclamacao GET    /reclamacoes/:id(.:format)                                                               reclamacoes#show
-#                                       PATCH  /reclamacoes/:id(.:format)                                                               reclamacoes#update
-#                                       PUT    /reclamacoes/:id(.:format)                                                               reclamacoes#update
-#                                       DELETE /reclamacoes/:id(.:format)                                                               reclamacoes#destroy
-#                           visit_index GET    /visit/index(.:format)                                                                   visit#index
-#                            home_index GET    /home/index(.:format)                                                                    home#index
-#                         welcome_index GET    /welcome/index(.:format)                                                                 welcome#index
-#                                  root GET    /                                                                                        reclamacoes#index
+#                                  root GET    /                                                                                        perfis#new
+#                                perfis POST   /perfis(.:format)                                                                        perfis#create
+#                            new_perfil GET    /perfis/new(.:format)                                                                    perfis#new
+#                                perfil GET    /perfis/:id(.:format)                                                                    perfis#show
+#                           vuejs_index GET    /vuejs/index(.:format)                                                                   vuejs#index
+#                        paperkit_index GET    /paperkit/index(.:format)                                                                paperkit#index
+#                      paperkit_landing GET    /paperkit/landing(.:format)                                                              paperkit#landing
+#                      paperkit_profile GET    /paperkit/profile(.:format)                                                              paperkit#profile
+#                     paperkit_register GET    /paperkit/register(.:format)                                                             paperkit#register
 #         rails_mandrill_inbound_emails POST   /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#create
 #         rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
 #            rails_relay_inbound_emails POST   /rails/action_mailbox/relay/inbound_emails(.:format)                                     action_mailbox/ingresses/relay/inbound_emails#create
@@ -38,7 +35,7 @@ Rails.application.routes.draw do
 
   root 'perfis#new'
 
-  resources :perfis, only: [:new, :show]
+  resources :perfis, only: [:show, :new, :create]
 
   # Exemplo
   # Rodando Vuejs
