32438b876b5adc3698754d542290bd045e3ae0b2
Marco Castro <marcodotcastro@gmail.com>
2019-10-23 10:47:27 -0300

    [a] annotate

--- app/controllers/perfis_controller.rb
+++ app/controllers/perfis_controller.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: perfis
+#
+#  id         :bigint           not null, primary key
+#  avatar     :string(255)
+#  descricao  :string
+#  github     :string(255)
+#  nome       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class PerfisController < ApplicationController
 
   def new
--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -3,7 +3,9 @@
 # Table name: perfis
 #
 #  id         :bigint           not null, primary key
+#  avatar     :string(255)
 #  descricao  :string
+#  github     :string(255)
 #  nome       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
