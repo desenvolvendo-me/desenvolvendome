70e232cdcffe03269e58434b04345aebd0f7c281
Marco Castro <marcodotcastro@gmail.com>
2019-10-24 15:00:36 -0300

    [r] importar repositório

--- app/businesses/import_user.rb
+++ app/businesses/import_user.rb
@@ -20,21 +20,41 @@ class ImportUser
 
   def repositories
     @github.repos(@user.login).each do |repo|
-      repository = @github.repo(@user.login, repo['name'])
-      commits = nil
-      @github.commits(@user.login, repo['name']).each do |contribuidor|
-        if contribuidor['login'] == @user.login
-          commits = contribuidor
-        end
-      end
-      @user.repositories << Repository.create(github_id: repository['id'],
-                                              name: repository['name'],
-                                              fork: repository['fork'],
-                                              forks_count: repository['forks_count'],
-                                              stargazers_count: repository['stargazers_count'],
-                                              commits_count: commits ? commits['contributions'] : 0,
-                                              size: repository['size'])
+      repo = repository(repo)
+      commits = commits(repo)
+      repository = Repository.create(github_id: repo['id'],
+                                     name: repo['name'],
+                                     fork: repo['fork'],
+                                     forks_count: repo['forks_count'],
+                                     stargazers_count: repo['stargazers_count'],
+                                     commits_count: commits ? commits['contributions'] : 0,
+                                     size: repo['size'])
+      languages(repo, repository)
+      @user.repositories << repository
     end
+  end
+
+  private
+
+  def languages(repo, repository)
+    @github.languages(@user.login, repo['name']).each do |language|
+      lang = Language.find_or_create_by(description: language[0])
+      repository.knowledges << Knowledge.create(exercise: language[1], language: lang)
+    end
+  end
+
+  def commits(repo)
+    commits = nil
+    @github.commits(@user.login, repo['name']).each do |contribuidor|
+      if contribuidor['login'] == @user.login
+        commits = contribuidor
+      end
+    end
+    commits
+  end
+
+  def repository(repo)
+    @github.repo(@user.login, repo['name'])
   end
 
 end
