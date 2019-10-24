78eaecfacece4a3405afb472a37a607254bc27aa
Marco Castro <marcodotcastro@gmail.com>
2019-10-24 09:43:09 -0300

    [c] token github

--- app/businesses/github.rb
+++ app/businesses/github.rb
@@ -9,17 +9,17 @@ class Github
   end
 
   def user(login)
-    response = @connect.get "/users/#{login}"
+    response = @connect.get "/users/#{login}?access_token=d2df4752c119c368fababddabaf963f05c07f015"
     JSON.parse(response.body)
   end
 
   def repos(login)
-    response = @connect.get "/users/#{login}/repos"
+    response = @connect.get "/users/#{login}/repos?access_token=d2df4752c119c368fababddabaf963f05c07f015"
     JSON.parse(response.body)
   end
 
   def repo(login, repo)
-    response = @connect.get "/repos/#{login}/#{repo}"
+    response = @connect.get "/repos/#{login}/#{repo}?access_token=d2df4752c119c368fababddabaf963f05c07f015"
     JSON.parse(response.body)
   end
 
