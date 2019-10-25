6781a4c9963c833f22cbb19d94896274094a692f
Marco Castro <marcodotcastro@gmail.com>
2019-10-25 15:03:59 -0300

    [a] label por porcentagem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def define_label(percentage)
+    case percentage
+    when 0..20
+      "danger"
+    when 20..50
+      "warning"
+    when 50..70
+      "info"
+    when 70..90
+      "success"
+    else
+      "default" #black
+    end
+  end
 end
