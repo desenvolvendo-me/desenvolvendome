c206a3043505975fd98e3bb1a8a49f147026166f
Marco Castro <marcodotcastro@gmail.com>
2019-10-29 08:55:21 -0300

    [a] adicionado email sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.sendgrid.net",
+      port: 587,
+      domain: ENV["DOMAIN_NAME"],
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["SENDGRID_USERNAME"],
+      password: ENV["SENDGRID_PASSWORD"]
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = {:host => ENV["DOMAIN_NAME"]}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 end
