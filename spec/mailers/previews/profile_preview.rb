557d1eee62ec7f219c831f6e4ad955957aca71fe
Marco Castro <marcodotcastro@gmail.com>
2019-10-29 09:28:48 -0300

    [c] email preview

--- spec/mailers/previews/profile_preview.rb
+++ spec/mailers/previews/profile_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/profile
 class ProfilePreview < ActionMailer::Preview
 
+  def evaluation_completed
+    ProfileMailer.with(user: User.last).evaluation_completed
+  end
+
 end
