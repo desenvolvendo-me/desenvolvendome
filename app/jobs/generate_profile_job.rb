b1906b59c52ff495b4e8596f3eba113f24cc5ed5
Marco Castro <marcodotcastro@gmail.com>
2019-10-30 13:51:59 -0300

    [r] isolando comportamento

--- app/jobs/generate_profile_job.rb
+++ app/jobs/generate_profile_job.rb
@@ -1,14 +1,26 @@
 require_relative '../../app/businesses/import/github'
-require_relative '../../app/businesses/profile/know'
-require_relative '../../app/businesses/profile/score'
+require_relative '../../app/businesses/profile/generate'
 
 class GenerateProfileJob < ApplicationJob
   queue_as :default
 
   def perform(user)
+    import_github(user)
+    profile_generate(user)
+    send_profile(user)
+  end
+
+  def import_github(user)
     Import::Github.new.run user
-    Profile::Know.new.run user
-    Profile::Score.new.run user
+  end
+
+  def profile_generate(user)
+    Profile::Generate.new(user).kown
+    Profile::Generate.new(user).evaluation
+    Profile::Generate.new(user).score
+  end
+
+  def send_profile(user)
     ProfileMailer.with(user: user).evaluation_completed.deliver_now
   end
 end
