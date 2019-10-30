999a710c63cc86269f70c275d913d071e45f1f19
Marco Castro <marcodotcastro@gmail.com>
2019-10-30 13:52:19 -0300

    [r] rspec

--- spec/businesses/profile/score_spec.rb
+++ spec/businesses/profile/score_spec.rb
@@ -4,22 +4,20 @@ RSpec.describe "GenerateScore" do
 
   before(:all) do
     @user = create(:user, :with_repository)
-    Profile::Know.new.run(@user)
+    Profile::Generate.new(@user).kown
   end
 
   it "score" do
-    Profile::Score.new.run(@user)
+    Profile::Generate.new(@user).score
 
     expect(@user.profile.score).to eq(2.2)
-    evaluation = @user.profile.evaluations.first
-    expect(evaluation.level).to eq(1)
-    expect(evaluation.evaluation_type).to eq("influence_digital")
   end
 
-  it "score with followers" do
+  it "score with evaluation followers" do
     @user.update(followers: 150)
 
-    Profile::Score.new.run(@user)
+    Profile::Generate.new(@user).evaluation
+    Profile::Generate.new(@user).score
 
     expect(@user.profile.score).to eq(2.6)
     evaluation = @user.profile.evaluations.first
