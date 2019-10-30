6e717561261df854de59c268179eb8d4e62e1115
Marco Castro <marcodotcastro@gmail.com>
2019-10-30 13:08:27 -0300

    [c] evaluation

---
+++ spec/factories/evaluations.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: evaluations
+#
+#  id              :bigint           not null, primary key
+#  evaluation_type :integer
+#  level           :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  profile_id      :bigint           not null
+#
+# Indexes
+#
+#  index_evaluations_on_profile_id  (profile_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (profile_id => profiles.id)
+#
+
+FactoryBot.define do
+  factory :evaluation do
+    level {1}
+    evaluation_type {:influence_digital}
+  end
+end
