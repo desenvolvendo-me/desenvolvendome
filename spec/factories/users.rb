b584f2c7a635d498accab6c1955d65886b36aa50
Marco Castro <marcodotcastro@gmail.com>
2019-11-01 11:45:24 -0300

    [a] basico usuário

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -30,8 +30,8 @@ FactoryBot.define do
     name {"Marco"}
     bio {"Pai da Luísa, Empreendedor e Engenheiro de Software. Fã do ecossistema do Ruby e Rails, mas trabalho também com PHP, Java e Javascript."}
     avatar {"https://avatars2.githubusercontent.com/u/15907252?s=460&v=4"}
-    followers {1}
-    following {1}
+    followers {0}
+    following {0}
 
     trait :with_repositories do
       after(:create) do |user|
