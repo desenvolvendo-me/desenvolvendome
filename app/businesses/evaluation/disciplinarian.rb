794814d573818173786e4e77f69a179c324909f5
Marco Castro <marcodotcastro@gmail.com>
2019-11-03 13:35:54 -0300

    [c] displicanador

---
+++ app/businesses/evaluation/disciplinarian.rb
@@ -0,0 +1,77 @@
+class Evaluation::Disciplinarian
+
+  def initialize(user)
+    @user = user
+  end
+
+  def run
+    create_evaluation
+  end
+
+  def level
+    case commits_index
+    when 0
+      level = 0
+    when 1..2
+      level = 1
+    when 2..3
+      level = 2
+    when 3..4
+      level = 3
+    when 4..5
+      level = 4
+    else
+      level = 5
+    end
+    level
+  end
+
+  def score
+    0.2 * level
+  end
+
+  private
+
+  def commits_index
+    index = 0
+    repositories = @user.repositories
+
+    commits_250 = repositories.where("commits_count >= 250 and commits_count < 500").count
+    commits_500 = repositories.where("commits_count >= 500 and commits_count < 1000").count
+    commits_1000 = repositories.where("commits_count >= 1000 and commits_count < 2000").count
+    commits_2000 = repositories.where("commits_count >= 2000 and commits_count < 5000").count
+    commits_5000 = repositories.where("commits_count >= 5000").count
+
+    if commits_250 > 0
+      index += 1 + (0.1 * commits_250)
+    end
+
+    if commits_500 > 0
+      index += 2 + (0.2 * commits_500)
+    end
+
+    if commits_1000 > 0
+      index += 3 + (0.3 * commits_1000)
+    end
+
+    if commits_2000 > 0
+      index += 4 + (0.4 * commits_2000)
+    end
+
+    if commits_5000 > 0
+      index += 5 + (0.5 * commits_5000)
+    end
+
+    index
+  end
+
+  def create_evaluation
+    evaludation = @user.profile.evaluations.where(evaluation_type: :disciplinarian).take
+    if evaludation
+      evaludation.update(level: level)
+    else
+      Evaluation.create(level: level, evaluation_type: :disciplinarian, profile: @user.profile)
+    end
+  end
+
+end
