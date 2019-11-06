2abc7a90d693ab32a8e038a890a7230058af6c43
Marco Castro <marcodotcastro@gmail.com>
2019-11-06 10:09:20 -0300

    [c] avaliações

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   before_action :find_user, only: [:create]
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
