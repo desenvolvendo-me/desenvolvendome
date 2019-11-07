1df994ed1bd05a0ad48deb0a174b53690ed51b9e
Marco Castro <marcodotcastro@gmail.com>
2019-11-07 16:51:43 -0300

    [a] view progress bar

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -4,4 +4,8 @@ module ProfileHelper
     profile.evaluations.pluck(:level, :evaluation_type).map {|level, evaluation_type| {level: level, evaluation_type: t("enum.evaluation_type.#{evaluation_type}")}}
   end
 
+  def progress_bar(user)
+    (1 - (user.profile.processing.to_f / user.repositories_count.to_f)) * 100
+  end
+
 end
