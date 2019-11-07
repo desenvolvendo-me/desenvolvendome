e63b0488e9c72bfc3ab659ce09394b99d1c5f2f7
Marco Castro <marcodotcastro@gmail.com>
2019-11-07 14:30:56 -0300

    [c] callback omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,4 +47,14 @@ class User < ApplicationRecord
   def slug_name
     "#{SecureRandom.hex[0..10]}"
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.login = auth.info.nickname
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name # assuming the user model has a name
+      user.avatar = auth.info.image # assuming the user model has an image
+    end
+  end
 end
