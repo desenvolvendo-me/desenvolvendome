c772ba0c5080c129cdd343d93fee88d7c79a0441
Marco Castro <marcodotcastro@gmail.com>
2019-11-07 14:45:53 -0300

    [a] avaliação

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      # GenerateProfileJob.perform_later @user
+      GenerateProfileJob.perform_later @user
 
       redirect_to confirmation_user_path
     else
