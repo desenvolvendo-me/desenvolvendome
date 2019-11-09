03bbd7b8632d9a5f6da2eaca1c51009702bd4dd2
Marco Castro <marcodotcastro@gmail.com>
2019-11-09 14:54:03 -0300

    [a] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,21 @@ FactoryBot.create(:user, :with_repositories, login: "desenvolvendome", email: "d
 puts "User: marcodotcastro, Score: 0.0"
 GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "marcodotcastro", email: "marcodotcastro@gmail.com"))
 
-# puts "User: leonardoscorza, Score: 0.6"
-# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "leonardoscorza", email: "leonardoscorza@gmail.com"))
-#
-# puts "User: lucascaton, Score: 3.8"
-# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "lucascaton", email: "lucascaton@gmail.com"))
-#
-# puts "User: rafaelfranca, Score: 3.6" #Score: 3
-# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "rafaelfranca", email: "rafaelfranca@gmail.com"))
-#
-# puts "User: kamranahmedse, Score: 4.2"
-# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "kamranahmedse", email: "kamranahmedse@gmail.com"))
+puts "Amigos"
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "MoacirPetry", email: "MoacirPetry@gmail.com"))
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "NickolasDev", email: "NickolasDev@gmail.com"))
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "pvm", email: "pvm@gmail.com"))
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "phalmeida", email: "phalmeida@gmail.com"))
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "leonnleite", email: "leonnleite@gmail.com"))
+
+puts "User: leonardoscorza, Score: 0.6"
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "leonardoscorza", email: "leonardoscorza@gmail.com"))
+
+puts "User: lucascaton, Score: 3.8"
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "lucascaton", email: "lucascaton@gmail.com"))
+
+puts "User: rafaelfranca, Score: 3.6" #Score: 3
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "rafaelfranca", email: "rafaelfranca@gmail.com"))
+
+puts "User: kamranahmedse, Score: 4.2"
+GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "kamranahmedse", email: "kamranahmedse@gmail.com"))
