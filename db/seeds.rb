db4f98bd759ce4b28816a58df225a92800c0ee9a
Marco Castro <marcodotcastro@gmail.com>
2019-11-09 14:15:09 -0300

    [a] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
+User.destroy_all
+
 puts "User: desenvolvendome, Score: 0"
-FactoryBot.create(:user, :with_repositories, email: "desenvolvendo@gmail.com")
+FactoryBot.create(:user, :with_repositories, login: "desenvolvendome", email: "desenvolvendo@gmail.com")
 
-puts "User: leonardoscorza, Score: 0.6"
-GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "leonardoscorza", email: "leonardoscorza@gmail.com"))
-
-#FIXME: Pq os três tem a mesma nota?
-puts "User: marcodotcastro, Score: 1.2" #Score: 1
+puts "User: marcodotcastro, Score: 0.0"
 GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "marcodotcastro", email: "marcodotcastro@gmail.com"))
 
-puts "User: lucascaton, Score: 2.2" #Score: 2
-GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "lucascaton", email: "lucascaton@gmail.com"))
-
-puts "User: rafaelfranca, Score: 2.2" #Score: 3
-GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "rafaelfranca", email: "rafaelfranca@gmail.com"))
-
-puts "User: kamranahmedse, Score: 4.6"
-GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "kamranahmedse", email: "kamranahmedse@gmail.com"))
+# puts "User: leonardoscorza, Score: 0.6"
+# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "leonardoscorza", email: "leonardoscorza@gmail.com"))
+#
+# puts "User: lucascaton, Score: 3.8"
+# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "lucascaton", email: "lucascaton@gmail.com"))
+#
+# puts "User: rafaelfranca, Score: 3.6" #Score: 3
+# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "rafaelfranca", email: "rafaelfranca@gmail.com"))
+#
+# puts "User: kamranahmedse, Score: 4.2"
+# GenerateProfileJob.perform_later(FactoryBot.create(:user, login: "kamranahmedse", email: "kamranahmedse@gmail.com"))
