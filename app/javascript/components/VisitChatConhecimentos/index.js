2967bf9b58855b5a543e30d44e652a409f1c587e
Marco Castro <marcodotcastro@gmail.com>
2019-11-10 12:08:53 -0300

    [a] componente component

--- app/javascript/components/VisitChatConhecimentos/index.js
+++ app/javascript/components/VisitChatConhecimentos/index.js
@@ -1,50 +1,37 @@
-import { Card } from 'antd';
+import {Card} from 'antd';
 import React from 'react';
-import { Cell, Pie, PieChart, Legend,Tooltip } from 'recharts';
+import {Cell, Pie, PieChart, Legend, Tooltip} from 'recharts';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-const RADIAN = Math.PI / 180;   
+const COLORS = ['#27ae60', '#DAF7A6', '#FFC300', '#FF5733', '#C70039', '#900C3F'];
+const RADIAN = Math.PI / 180;
 
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
- const x  = cx + radius * Math.cos(-midAngle * RADIAN);
- const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
- return (
-   <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
-     {`${(percent * 100).toFixed(0)}%`}
-   </text>
- );
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
 };
 
 
 export default function VisitChatConhecimentos(props) {
-  const analise_data = [
-    {
-      name: 'HTML', value: 70,
-    },
-    {
-      name: 'CSS', value: 16,
-    },
-    {
-      name: 'Ruby', value: 11,
-    },
-    {
-      name: 'Outros', value: 15,
-    }
-  ];
-  
-  return (
-    <Card title="Conhecimentos" bordered={false}>
-      <PieChart width={400} height={400}>
-        <Pie data={analise_data} dataKey="value" fill="#8884d8" labelLine={false} label={renderCustomizedLabel}>
-          {
-          	analise_data.map((entry, index) => <Cell key={entry.name} fill={COLORS[index % COLORS.length]}/>)
-          }
-        </Pie>
-        <Legend />
-        <Tooltip />
-      </PieChart>
-    </Card>
-  );
+    console.log(props)
+    return (
+        <Card title="Conhecimentos" bordered={false}>
+            <PieChart width={400} height={400}>
+                <Pie data={props.data} dataKey="value" fill="#8884d8" labelLine={false} label={renderCustomizedLabel}>
+                    {
+                        props.data.map((entry, index) => <Cell key={entry.name}
+                                                               fill={COLORS[index % COLORS.length]}/>)
+                    }
+                </Pie>
+                <Legend/>
+                <Tooltip/>
+            </PieChart>
+        </Card>
+    );
 }
