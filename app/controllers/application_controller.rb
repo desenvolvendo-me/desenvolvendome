1c5f78cbf4a9767d3ec9cf0ffcc156f8ddbf6cde
Marco Castro <marcodotcastro@gmail.com>
2019-11-14 09:08:47 -0300

    [a] capture exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :handle_exception
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: 1) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+
+  def handle_exception(exception)
+    Raven.capture_exception(exception)
+    raise exception
+  end
 end
