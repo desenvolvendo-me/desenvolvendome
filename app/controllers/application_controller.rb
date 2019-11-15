2bdacc73fab0a34cb994b0291b7c51930184ea83
Marco Castro <marcodotcastro@gmail.com>
2019-11-15 14:26:43 -0300

    [f] sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception, with: :handle_exception
-  before_action :set_raven_context
-
-  private
-
-  def set_raven_context
-    Raven.user_context(id: 1) # or anything else in session
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
-
-  def handle_exception(exception)
-    #NOTE: para não usar o sentry em development local, somente na digitalocean já que hoje estamos usando em modo development
-    Raven.capture_exception(exception) if request.original_url.include? "0.0.0.0"
-    raise exception
-  end
 end
--- app/jobs/sentry_job.rb
+++ app/jobs/sentry_job.rb
@@ -2,7 +2,7 @@ class SentryJob < ActiveJob::Base
   queue_as :sentry
 
   def perform(event)
-    Raven.send_event(event)
+    Raven.send_event(event) if event["request"]['url'].include? "0.0.0.0"
   end
 end
 
