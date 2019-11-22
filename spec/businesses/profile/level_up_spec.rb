0334b570bca1a1db2e691955d532c612458e7cc8
Marco Castro <marcodotcastro@gmail.com>
2019-11-22 17:14:33 -0300

    [a] rspec

--- spec/businesses/profile/level_up_spec.rb
+++ spec/businesses/profile/level_up_spec.rb
@@ -77,6 +77,114 @@ RSpec.describe "LevelUp" do
       expect(@user.repositories.sum(:commits_count)).to eq(656)
     end
 
+    it 'marcodotcastro' do
+      create(:repository, commits_count: 48, user: @user)
+      create(:repository, commits_count: 68, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 4, user: @user)
+      create(:repository, commits_count: 6, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 39, user: @user)
+      create(:repository, commits_count: 88, user: @user)
+      create(:repository, commits_count: 61, user: @user)
+      create(:repository, commits_count: 212, user: @user)
+      create(:repository, commits_count: 2, user: @user)
+      create(:repository, commits_count: 3, user: @user)
+      create(:repository, commits_count: 86, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 39, user: @user)
+
+      Profile::LevelUp.new(@user).run
+
+      expect(@user.profile.evaluation.evaluation_type).to eq("novice")
+      expect(@user.profile.evaluation.level).to eq(10)
+      expect(@user.repositories.sum(:commits_count)).to eq(656)
+    end
+
+    it 'MoacirPetry' do
+      create(:repository, commits_count: 48, user: @user)
+      create(:repository, commits_count: 5, user: @user)
+      create(:repository, commits_count: 25, user: @user)
+      create(:repository, commits_count: 4, user: @user)
+      create(:repository, commits_count: 4, user: @user)
+      create(:repository, commits_count: 15, user: @user)
+      create(:repository, commits_count: 5, user: @user)
+      create(:repository, commits_count: 29, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 26, user: @user)
+      create(:repository, commits_count: 124, user: @user)
+
+      Profile::LevelUp.new(@user).run
+
+      expect(@user.profile.evaluation.evaluation_type).to eq("started")
+      expect(@user.profile.evaluation.level).to eq(10)
+      expect(@user.repositories.sum(:commits_count)).to eq(285)
+    end
+
+    it 'leonnleite' do
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 21, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 2, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 2, user: @user)
+      create(:repository, commits_count: 6, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 6, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 2, user: @user)
+      create(:repository, commits_count: 3, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 15, user: @user)
+      create(:repository, commits_count: 4, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 2, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 5, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 3, user: @user)
+      create(:repository, commits_count: 9, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 3, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 2, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 0, user: @user)
+      create(:repository, commits_count: 1, user: @user)
+
+      Profile::LevelUp.new(@user).run
+
+      expect(@user.profile.evaluation.evaluation_type).to eq("started")
+      expect(@user.profile.evaluation.level).to eq(7)
+      expect(@user.repositories.sum(:commits_count)).to eq(94)
+    end
+
   end
 
 
