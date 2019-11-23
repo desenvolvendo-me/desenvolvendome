4a79b8ba7f200252277869ac3968850184fbdf5b
Marco Castro <marcodotcastro@gmail.com>
2019-11-23 10:19:46 -0300

    [c] regra tipo de conhecimento

--- app/businesses/evaluation/know.rb
+++ app/businesses/evaluation/know.rb
@@ -26,9 +26,9 @@ class Evaluation::Know
   def merge_knowledge(exercise_total, technology)
     language_exist = @user.profile.knowledges.joins(:language).where("languages.id = ?", technology.language.id).last
     if language_exist
-      language_exist.update(experience: calc_experience_per_technology(exercise_total, technology))
+      language_exist.update(experience: calc_experience_per_technology(exercise_total, technology), knowledge_type: set_knowledge_type(technology.language))
     else
-      @user.profile.knowledges << Knowledge.create(experience: calc_experience_per_technology(exercise_total, technology), language: technology.language)
+      @user.profile.knowledges << Knowledge.create(experience: calc_experience_per_technology(exercise_total, technology), language: technology.language, knowledge_type: set_knowledge_type(technology.language))
     end
   end
 
@@ -37,7 +37,7 @@ class Evaluation::Know
   end
 
   def technologies
-    Technology.joins(:language, repository: [:user]).where("repositories.commits_count > 100 and users.id = ?", @user.id)
+    Technology.joins(:language, repository: [:user]).where("users.id = ?", @user.id)
   end
 
   def experience(technology)
@@ -50,6 +50,29 @@ class Evaluation::Know
     end
 
     @experience_per_language[language][1]
+  end
+
+  def set_knowledge_type(language)
+    return :basic if basic.include? language.description
+    return :normal if normal.include? language.description
+    return :rare if rare.include? language.description
+    return :special if special.include? language.description
+  end
+
+  def basic
+    ["HTML", "CSS", "JavaScript"]
+  end
+
+  def normal
+    ["Java", "Python", "Ruby", "PHP", "C#", "TypeScript"]
+  end
+
+  def rare
+    ["XSLT", "R", "C", "C++", "Shell", "PowerShell", "Groovy", "Dockerfile"]
+  end
+
+  def special
+    ["Swift", "Objective-C ", "Go", "Elixir", "Erlang"]
   end
 
 end
