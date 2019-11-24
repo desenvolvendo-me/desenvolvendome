d69e4980127a93aac1e5c73edb732871de217cfc
Marco Castro <marcodotcastro@gmail.com>
2019-11-24 09:37:22 -0300

    [f] know

--- app/businesses/evaluation/know.rb
+++ app/businesses/evaluation/know.rb
@@ -1,74 +1,62 @@
-#TODO: Refatoring
 class Evaluation::Know
+  #TODO:Informação que devem ir para o modulo admin
+  @technologies = nil
+  SETTINGS = {
+      commits_level: 25,
+      languages: {
+          basic: ["HTML", "CSS", "JavaScript", "Shell", "PowerShell"],
+          normal: ["Java", "Python", "Ruby", "PHP", "C#", "TypeScript"],
+          rare: ["C", "C++", "Groovy", "Dockerfile", "Vue"],
+          special: ["Swift", "Objective-C ", "Kotlin", "Go", "Elixir", "Erlang"]}
+  }
 
   def initialize(user)
     @user = user
+    @technologies = get_technologies
   end
 
   def run
-    update_knowledges
+    set_knowledges
   end
 
   private
 
-  def update_knowledges
-    set_profile
-    technologies.each(&method(:merge_knowledge))
+  def set_knowledges
+    @technologies.each(&method(:merge_knowledge))
   end
 
   def merge_knowledge(technology)
-    knowledge_with_language_exist = @user.profile.knowledges.joins(:language).where("languages.id = ?", technology.language.id).last
+    language = technology.language
 
-    if knowledge_with_language_exist
-      knowledge_with_language_exist.update(experience: technology.exercise, knowledge_type: set_knowledge_type(technology.language), level: level(technology.language))
+    if (knowledge_with_language = knowledge_with_language(language))
+      knowledge_with_language.update(experience: technology.exercise, knowledge_type: set_knowledge_type(language), level: get_level(language))
     else
-      @user.profile.knowledges << Knowledge.create(experience: technology.exercise, language: technology.language, knowledge_type: set_knowledge_type(technology.language), level: level(technology.language))
+      Knowledge.create(profile: @user.profile, experience: technology.exercise, language: language, knowledge_type: set_knowledge_type(language), level: get_level(language))
     end
   end
 
-  def set_profile
-    @user.profile = Profile.create(score: 0) unless @user.profile
-  end
-
-  def technologies
-    Technology.joins(:language, repository: [:user]).where("users.id = ?", @user.id)
+  def knowledge_with_language(language)
+    @user.profile.knowledges.joins(:language).where("languages.id = ?", language.id).take
   end
 
   def set_knowledge_type(language)
-    knowledge_type = nil
-    knowledge_type = :basic if basic.include? language.description
-    knowledge_type = :normal if normal.include? language.description
-    knowledge_type = :rare if rare.include? language.description
-    knowledge_type = :special if special.include? language.description
+    knowledge_type = :basic if SETTINGS[:languages][:basic].include? language.description
+    knowledge_type = :normal if SETTINGS[:languages][:normal].include? language.description
+    knowledge_type = :rare if SETTINGS[:languages][:rare].include? language.description
+    knowledge_type = :special if SETTINGS[:languages][:special].include? language.description
     knowledge_type
   end
 
-  def level(language)
+  def get_technologies
+    Technology.joins(:language, repository: [:user]).where("users.id = ?", @user.id)
+  end
+
+  def get_level(language)
     commits = 0
-    technologies.where("languages.description = ?", language.description).each do |technology|
+    @technologies.where("languages.description = ?", language.description).each do |technology|
       commits += technology.repository.commits_count
     end
-    commits / settings[:commits_level]
+    commits / SETTINGS[:commits_level]
   end
 
-  #TODO:Informação que devem ir para o modulo admin
-  def basic
-    ["HTML", "CSS", "JavaScript", "Shell", "PowerShell"]
-  end
-
-  def normal
-    ["Java", "Python", "Ruby", "PHP", "C#", "TypeScript"]
-  end
-
-  def rare
-    ["C", "C++", "Groovy", "Dockerfile", "Vue"]
-  end
-
-  def special
-    ["Swift", "Objective-C ", "Kotlin", "Go", "Elixir", "Erlang"]
-  end
-
-  def settings
-    {commits_level: 25}
-  end
 end
