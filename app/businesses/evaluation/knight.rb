68b943aab384ef1f820fc95643d0ff6bbc1c0f2e
Marco Castro <marcodotcastro@gmail.com>
2019-11-24 08:22:59 -0300

    [c] constants

--- app/businesses/evaluation/knight.rb
+++ app/businesses/evaluation/knight.rb
@@ -1,5 +1,6 @@
-#TODO: Refatoring
 class Evaluation::Knight
+  DESCRIPTION_MAX_LEVEL = "MÁXIMO NÍVEL"
+  SETTING = {min_level: 1, max_level: 10, role: {kind: :knight, calc: 300, repositories: 3, commits: 3000, repository_size: 500, commits_preview: 300}}
 
   def initialize(user)
     @user = user
@@ -10,21 +11,19 @@ class Evaluation::Knight
   end
 
   def level
-    lv = 0
-    lv = 1 + ((commits - role[:commits_preview]) / role[:calc]) unless up?
-    lv = 10 if max
+    lv = SETTING[:min_level] + ((commits - SETTING[:role][:commits_preview]) / SETTING[:role][:calc]) unless up?
+    lv = SETTING[:max_level] if max
     lv
   end
 
   def up?
-    # (repositories >= role[:repositories] and commits >= role[:commits])
-    false #TODO: Desabilitado level up, pois não tem um próximo nível
+    false
   end
 
   def next_level
     level = {number: 0, percentage: 0}
-    level[:number] = max ? "MÁXIMO NÍVEL" : "+#{xp_next_level}"
-    level[:percentage] = max ? 100 : (1 - (xp_next_level.to_f / role[:calc].to_f)) * 100
+    level[:number] = max ? DESCRIPTION_MAX_LEVEL : "+#{xp_next_level}"
+    level[:percentage] = max ? 100 : (1 - (xp_next_level.to_f / SETTING[:role][:calc].to_f)) * 100
     level
   end
 
@@ -34,9 +33,9 @@ class Evaluation::Knight
     evaludation = @user.profile.evaluation
 
     if evaludation
-      evaludation.update(level: level, evaluation_type: :knight)
+      evaludation.update(level: level, evaluation_type: SETTING[:role][:kind])
     else
-      Evaluation.create(level: level, evaluation_type: :knight, profile: @user.profile)
+      Evaluation.create(level: level, evaluation_type: SETTING[:role][:kind], profile: @user.profile)
     end
   end
 
@@ -45,19 +44,15 @@ class Evaluation::Knight
   end
 
   def repositories
-    @user.repositories.where("commits_count >= #{role[:repository_size]}").count
+    @user.repositories.where("commits_count >= #{SETTING[:role][:repository_size]}").count
   end
 
   def xp_next_level
-    role[:calc] - (commits % role[:calc])
+    SETTING[:role][:calc] - (commits % SETTING[:role][:calc])
   end
 
   def max
-    commits >= role[:commits]
-  end
-
-  def role
-    {calc: 300, repositories: 3, commits: 3000, repository_size: 500, commits_preview: 300}
+    commits >= SETTING[:role][:commits]
   end
 
 end
--- app/businesses/evaluation/novice.rb
+++ app/businesses/evaluation/novice.rb
@@ -1,5 +1,6 @@
-#TODO: Refatoring
 class Evaluation::Novice
+  DESCRIPTION_MAX_LEVEL = "MÁXIMO NÍVEL"
+  SETTING = {min_level: 1, max_level: 10, role: {kind: :novice, calc: 30, repositories: 3, commits: 300, repository_size: 100, commits_preview: 150}}
 
   def initialize(user)
     @user = user
@@ -10,20 +11,19 @@ class Evaluation::Novice
   end
 
   def level
-    lv = 0
-    lv = 1 + ((commits - role[:commits_preview]) / role[:calc]) unless up?
-    lv = 10 if max
+    lv = SETTING[:min_level] + ((commits - SETTING[:role][:commits_preview]) / SETTING[:role][:calc]) unless up?
+    lv = SETTING[:max_level] if max
     lv
   end
 
   def up?
-    (repositories >= role[:repositories] and commits >= role[:commits])
+    (repositories >= SETTING[:role][:repositories] and commits >= SETTING[:role][:commits])
   end
 
   def next_level
     level = {number: 0, percentage: 0}
-    level[:number] = max ? "MÁXIMO NÍVEL" : "+#{xp_next_level}"
-    level[:percentage] = max ? 100 : (1 - (xp_next_level.to_f / role[:calc].to_f)) * 100
+    level[:number] = max ? DESCRIPTION_MAX_LEVEL : "+#{xp_next_level}"
+    level[:percentage] = max ? 100 : (1 - (xp_next_level.to_f / SETTING[:role][:calc].to_f)) * 100
     level
   end
 
@@ -33,9 +33,9 @@ class Evaluation::Novice
     evaludation = @user.profile.evaluation
 
     if evaludation
-      evaludation.update(level: level, evaluation_type: :novice)
+      evaludation.update(level: level, evaluation_type: SETTING[:role][:kind])
     else
-      Evaluation.create(level: level, evaluation_type: :novice, profile: @user.profile)
+      Evaluation.create(level: level, evaluation_type: SETTING[:role][:kind], profile: @user.profile)
     end
   end
 
@@ -44,19 +44,15 @@ class Evaluation::Novice
   end
 
   def repositories
-    @user.repositories.where("commits_count >= #{role[:repository_size]}").count
+    @user.repositories.where("commits_count >= #{SETTING[:role][:repository_size]}").count
   end
 
   def xp_next_level
-    role[:calc] - (commits % role[:calc])
+    SETTING[:role][:calc] - (commits % SETTING[:role][:calc])
   end
 
   def max
-    commits >= role[:commits]
-  end
-
-  def role
-    {calc: 30, repositories: 3, commits: 300, repository_size: 100, commits_preview: 150}
+    commits >= SETTING[:role][:commits]
   end
 
 end
