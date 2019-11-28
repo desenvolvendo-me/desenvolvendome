1ea0fdb51b882c8d536dcb23c395cb2cf4b35ea4
Marco Castro <marcodotcastro@gmail.com>
2019-11-28 10:32:56 -0300

    [f] bug import

--- app/businesses/profile/evaluation/know.rb
+++ app/businesses/profile/evaluation/know.rb
@@ -54,7 +54,7 @@ class Profile::Evaluation::Know
   def get_level(language)
     commits = 0
     @technologies.where("languages.description = ?", language.description).each do |technology|
-      commits += technology.repository.commits_count
+      commits += technology.repository.commits_count.to_i #FIXME: Vindo nil
     end
     commits / SETTINGS[:commits_level]
   end
