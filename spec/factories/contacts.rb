d13a5fd8e0d459522bbc342b112e57c9771ccf58
Marco Castro <marcodotcastro@gmail.com>
2019-11-29 08:42:40 -0300

    [c] model contact

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id           :bigint           not null, primary key
+#  contact_type :integer
+#  description  :string
+#  email        :string
+#  name         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+FactoryBot.define do
+  factory :contact do
+    name { "MyString" }
+    email { "MyString" }
+    contact_type { 1 }
+    description { "MyString" }
+  end
+end
