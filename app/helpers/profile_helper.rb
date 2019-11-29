1c923d2d76c6e415055b18f9972690f4ad982c41
Marco Castro <marcodotcastro@gmail.com>
2019-11-29 08:57:00 -0300

    [e] method

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,13 +1,5 @@
 module ProfileHelper
 
-  def result_evaluations(profile)
-    profile.evaluations.pluck(:level, :evaluation_type).map {|level, evaluation_type| {level: level, evaluation_type: t("enum.evaluation_type.#{evaluation_type}")}}
-  end
-
-  def result_knowledges(profile)
-    profile.knowledges.joins(:language).where("experience >= 1").pluck(:experience, :"languages.description").first(5).map {|experience, language| {name: language, value: experience.round(2)}}
-  end
-
   def progress_bar(user)
     processing = user.profile.processing.to_f / user.repositories_count.to_f
     return 100 if processing == 1
