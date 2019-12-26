af22ca230defe3bbdc826ac0b93610653557d7f3
Marco Castro <marcodotcastro@gmail.com>
2019-12-26 09:06:26 -0300

    [a] list

--- config/initializers/rails_admin_custom.rb
+++ config/initializers/rails_admin_custom.rb
@@ -7,4 +7,10 @@ RailsAdmin.config do |config|
     end
   end
 
+  config.model "User" do
+    list do
+      include_fields :login, :name, :evaluations_count, :repositories_count, :followers, :following
+    end
+  end
+
 end
