a5a75292492f4141e2a675e69ea505790cebe8aa
Marco Castro <marcodotcastro@gmail.com>
2019-12-29 11:03:00 -0300

    [a] fields

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Contact do
   menu priority: 3
-  
+
+  index do
+    column :name
+    column :email
+    column :contact_type
+    column :description
+    column :created_at do |user|
+      user.created_at.strftime("%d/%m/%y %H:%M")
+    end
+  end
+
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,8 +2,8 @@ ActiveAdmin.register User do
   menu priority: 1
 
   index do
-    column :evaluation_type do |user|
-      link_to user.to_s, user_path(user.login), target: "_blank"
+    column :level do |user|
+      link_to user.level, user_path(user.login), target: "_blank"
     end
     column :login do |user|
       link_to user.login, "https://github.com/#{user.login}", target: "_blank"
@@ -13,6 +13,9 @@ ActiveAdmin.register User do
     column :following
     column "Evaluations", :evaluations_count
     column "Repositories", :repositories_count
+    column :created_at do |user|
+      user.created_at.strftime("%d/%m/%y %H:%M")
+    end
   end
 
 end
