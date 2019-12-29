81bff5c81cad9edae964e3210b23636547d9a151
Marco Castro <marcodotcastro@gmail.com>
2019-12-29 11:03:00 -0300

    [a] fields

--- app/admin/interests.rb
+++ app/admin/interests.rb
@@ -1,5 +1,11 @@
 ActiveAdmin.register Interest do
   menu priority: 2
 
+  index do
+    column :email
+    column :created_at do |user|
+      user.created_at.strftime("%d/%m/%y %H:%M")
+    end
+  end
   
 end
