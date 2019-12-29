4656a730a0485b78a1e2107a9cdf7003e37d516c
Marco Castro <marcodotcastro@gmail.com>
2019-12-29 11:22:03 -0300

    [a] dashboards

--- app/admin/dashboards/dashboard.rb
+++ app/admin/dashboards/dashboard.rb
@@ -1,38 +1,45 @@
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc {I18n.t("active_admin.dashboard")}
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
+  content title: proc {I18n.t("active_admin.dashboard")} do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span "Cadastros Recentes".upcase
+        span "Cadastros".upcase
       end
     end
     columns do
       column do
-        panel "Usuários" do
-          ul do
-            User.last(10).map do |user|
-              li link_to(user.to_s, user_path(user.login), target: "_blank")
-            end
-          end
+        panel "Usuários por Dia" do
+          render partial: 'dashboards/users/per_day'
         end
       end
       column do
-        panel "Interesses" do
-          ul do
-            Interest.last(10).map do |interest|
-              li link_to(interest.to_s, admin_interest_path(interest), target: "_blank")
-            end
-          end
+        panel "Usuários por Mês" do
+          render partial: 'dashboards/users/per_month'
+        end
+      end
+    end
+    columns do
+      column do
+        panel "Interesses por Dia" do
+          render partial: 'dashboards/interests/per_day'
         end
       end
       column do
-        panel "Contatos" do
-          ul do
-            Contact.last(10).map do |contact|
-              li link_to(contact.to_s, admin_contact_path(contact), target: "_blank")
-            end
-          end
+        panel "Interesses por Mês" do
+          render partial: 'dashboards/interests/per_month'
+        end
+      end
+    end
+    columns do
+      column do
+        panel "Contatos por Dia" do
+          render partial: 'dashboards/contacts/per_day'
+        end
+      end
+      column do
+        panel "Contatos por Mês" do
+          render partial: 'dashboards/contacts/per_month'
         end
       end
     end
