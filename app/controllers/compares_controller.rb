26e73ffab2af4fd1c9b49e04d5dcadda1e6b527e
Marco Castro <marcodotcastro@gmail.com>
2020-01-01 10:42:54 -0300

    [a] validação da comparação

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -16,7 +16,7 @@ class ComparesController < ApplicationController
         @compare.save!
         format.html {redirect_to compare_path(login_1: @compare.user.login, login_2: @compare.compared.login)}
       else
-        format.html {render :new}
+        format.html {redirect_to :compare_new, notice: alert_user_not_exist(user1, user2)}
       end
     end
   end
@@ -27,6 +27,30 @@ class ComparesController < ApplicationController
   end
 
   private
+
+  def manager_compare(user1, user2)
+    @compare = Compare.where(user: user1, compared: user2).take
+    if @compare
+      count = @compare.comparations_count.to_i + 1
+      @compare.update!(comparations_count: count)
+    else
+      @compare = Compare.new(user: user1, compared: user2)
+    end
+  end
+
+  def alert_user_not_exist(user1, user2)
+    if compare_params[:login_1].present? or compare_params[:login_2].present?
+      if !user1 and !user2
+        "Os dois logins não foram avaliados!"
+      elsif !user1
+        "O login <b>#{compare_params[:login_1]}</b> não foi avaliado!"
+      elsif !user2
+        "O login <b>#{compare_params[:login_2]}</b> não foi avaliado!"
+      end
+    else
+      "Nenhum login pode ficar vazio."
+    end
+  end
 
   def compare_params
     params.require(:compare).permit(:login_1, :login_2)
