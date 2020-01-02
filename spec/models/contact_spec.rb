d587358e50a64e10a37630e37f7c1f2015d82486
Marco Castro <marcodotcastro@gmail.com>
2020-01-02 16:20:14 -0300

    [a] annotation

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,6 +7,7 @@
 #  description  :text
 #  email        :string
 #  name         :string
+#  response     :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
