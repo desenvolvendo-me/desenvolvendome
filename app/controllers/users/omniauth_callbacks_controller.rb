b2d68f8869f71154c1e4b8223ec92f070a42caca
Marco Castro <marcodotcastro@gmail.com>
2020-01-07 16:34:15 -0300

    [a] login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
+
+      sign_in @user
+
       GenerateProfileJob.perform_later(@user.login)
 
       redirect_to user_path(@user.login)
