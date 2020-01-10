c63c501af344527204f5694bc71e9ee74a2455c9
Marco Castro <marcodotcastro@gmail.com>
2020-01-10 11:22:55 -0300

    [r] routes

--- config/routes.rb
+++ config/routes.rb
@@ -170,7 +170,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   require 'sidekiq/web'
-  authenticate :admin_user do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+  mount Sidekiq::Web => '/sidekiq'
+
 end
