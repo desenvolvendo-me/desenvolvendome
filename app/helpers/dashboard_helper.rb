820726cee8e7ecdd8985aa6da2771114e2e13f8e
Marco Castro <marcodotcastro@gmail.com>
2020-01-14 14:40:54 -0300

    [f] profiles acesses

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -8,13 +8,10 @@ module DashboardHelper
   end
 
   def self.per_page_profile_between(number_accesses_min, number_accesses_max)
-    group_event_page = Ahoy::Event.group("properties -> 'page'").count
-    group_event_page = group_event_page.delete_if {|k, v| k.include?("profile") and (v < number_accesses_min or v > number_accesses_max)}
-    group_event_page = group_event_page.delete_if {|k, v| k.eql?("/")}
-    group_event_page = group_event_page.delete_if {|k, v| k.include?("analyze")}
-    group_event_page = group_event_page.delete_if {|k, v| k.include?("compare")}
-    group_event_page = group_event_page.delete_if {|k, v| k.include?("interests")}
-    group_event_page
+    events_page = Ahoy::Event.group("properties -> 'page'").count
+    events_profile = events_page.delete_if {|k, v| k.exclude?("profile")}
+    events_between = events_profile.delete_if {|k, v| (v < number_accesses_min or v > number_accesses_max)}
+    events_between.sort_by {|e| e.last}.reverse
   end
 
 end
