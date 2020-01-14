3b4cbb240760235849c1be38d25448a36ead71da
Marco Castro <marcodotcastro@gmail.com>
2020-01-14 12:23:35 -0300

    [a] relatório

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -7,10 +7,9 @@ module DashboardHelper
     group_event_page
   end
 
-  def self.per_page_profile_50
-    number_visits = 50
+  def self.per_page_profile_greater_than(number_accesses)
     group_event_page = Ahoy::Event.group("properties -> 'page'").count
-    group_event_page = group_event_page.delete_if {|k, v| k.include?("profile") and v < number_visits}
+    group_event_page = group_event_page.delete_if {|k, v| k.include?("profile") and v < number_accesses}
     group_event_page = group_event_page.delete_if {|k, v| k.eql?("/")}
     group_event_page = group_event_page.delete_if {|k, v| k.include?("analyze")}
     group_event_page = group_event_page.delete_if {|k, v| k.include?("compare")}
