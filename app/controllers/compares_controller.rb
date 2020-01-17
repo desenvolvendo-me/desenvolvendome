68c21ce19a0ec27fc19d896b9fd38083ec874961
Marco Castro <marcodotcastro@gmail.com>
2020-01-17 14:36:26 -0300

    [a] removido form

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -2,25 +2,8 @@
 class ComparesController < ApplicationController
 
   def new
-    @compare = Compare.new
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true).includes(:profile).page params[:page]
-  end
-
-  def create
-    user1 = User.find_by_login(compare_params[:login_1])
-    user2 = User.find_by_login(compare_params[:login_2])
-
-    manager_compare(user1, user2)
-
-    respond_to do |format|
-      if user1 and user2
-        @compare.save!
-        format.html {redirect_to compare_path(login_1: @compare.user.login, login_2: @compare.compared.login)}
-      else
-        format.html {redirect_to :compare_new, notice: alert_user_not_exist(user1, user2)}
-      end
-    end
   end
 
   def show
@@ -29,30 +12,6 @@ class ComparesController < ApplicationController
   end
 
   private
-
-  def manager_compare(user1, user2)
-    @compare = Compare.where(user: user1, compared: user2).take
-    if @compare
-      count = @compare.comparations_count.to_i + 1
-      @compare.update!(comparations_count: count)
-    else
-      @compare = Compare.new(user: user1, compared: user2)
-    end
-  end
-
-  def alert_user_not_exist(user1, user2)
-    if compare_params[:login_1].present? or compare_params[:login_2].present?
-      if !user1 and !user2
-        "Os dois logins não foram avaliados!"
-      elsif !user1
-        "O login <b>#{compare_params[:login_1]}</b> não foi avaliado!"
-      elsif !user2
-        "O login <b>#{compare_params[:login_2]}</b> não foi avaliado!"
-      end
-    else
-      "Nenhum login pode ficar vazio."
-    end
-  end
 
   def compare_params
     params.require(:compare).permit(:login_1, :login_2)
