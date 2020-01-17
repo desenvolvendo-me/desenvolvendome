19fb63d5b2138e88e63956bc813d290416659d52
Marco Castro <marcodotcastro@gmail.com>
2020-01-17 14:36:53 -0300

    [a] compare

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -7,15 +7,16 @@ class ComparesController < ApplicationController
   end
 
   def show
-    @user = User.find_by_login(params[:login_1])
-    @compared = User.find_by_login(params[:login_2])
+    @user = User.find_by_login(params[:user])
+    @compared = User.find_by_login(params[:compared])
+
+    Compare.create(user: @user, compared: @compared)
   end
 
   private
 
   def compare_params
-    params.require(:compare).permit(:login_1, :login_2)
+    params.require(:compare).permit(:user, :compared)
   end
-
 
 end
