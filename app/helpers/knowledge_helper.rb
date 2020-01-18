eab21b27ead096bd848a172e72bd4c3cf35fddab
Marco Castro <marcodotcastro@gmail.com>
2020-01-18 09:36:26 -0300

    [a] comparado com os usuários

---
+++ app/helpers/knowledge_helper.rb
@@ -0,0 +1,19 @@
+module KnowledgeHelper
+
+  def people_same_knowledge(knowledge)
+    profile_count = Profile.count
+    profile_count_with_knowledge = Profile.joins(:knowledges).where("knowledges.language_id": knowledge.language.id).count
+    percentage = ((profile_count_with_knowledge.to_f / profile_count.to_f) * 100).round(2)
+
+    "Pessoas que também conhecem <strong class='#{percentage < 20 ? "text-success" : "text-danger"}'>#{percentage}%</strong>".html_safe
+  end
+
+  def people_higher_level(knowledge)
+    profile_count = Profile.count
+    profile_count_with_knowledge_and_level = Profile.joins(:knowledges).where("knowledges.language_id": knowledge.language.id).where("knowledges.level > ?", knowledge.level).count
+    percentage = ((profile_count_with_knowledge_and_level.to_f / profile_count.to_f) * 100).round(2)
+
+    "Pessoas com nível maior <strong class='#{percentage < 20 ? "text-success" : "text-danger"}'>#{percentage}%</strong>".html_safe
+  end
+
+end
