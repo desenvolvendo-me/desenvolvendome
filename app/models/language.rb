732dde1c71c04b353c8398d62ab2fe9feb4297c6
Marco Castro <marcodotcastro@gmail.com>
2020-01-19 13:26:06 -0300

    [f] calc raridade da linguagem

--- app/models/language.rb
+++ app/models/language.rb
@@ -13,9 +13,30 @@
 class Language < ApplicationRecord
   paginates_per 24
 
+  KNOWLEDGE_TYPE = {
+      basic: ["HTML", "CSS", "JavaScript", "Shell", "PowerShell"],
+      normal: ["Java", "Python", "Ruby", "PHP", "C#", "TypeScript"],
+      rare: ["C", "C++", "Groovy", "Dockerfile", "Vue"],
+      special: ["Swift", "Objective-C ", "Kotlin", "Go", "Elixir", "Erlang"]
+  }
+
   enum knowledge_type: [:basic, :normal, :rare, :special, :unknown]
   enum rarity: [:very_low, :low, :medium, :high, :very_high]
 
   has_many :knowledges
   has_many :technologies
+
+
+  before_save :set_knowledge_type
+
+  private
+
+  def set_knowledge_type
+    knowledge_type = :unknown
+    knowledge_type = :basic if KNOWLEDGE_TYPE[:basic].include? self.description
+    knowledge_type = :normal if KNOWLEDGE_TYPE[:normal].include? self.description
+    knowledge_type = :rare if KNOWLEDGE_TYPE[:rare].include? self.description
+    knowledge_type = :special if KNOWLEDGE_TYPE[:special].include? self.description
+    self.knowledge_type = knowledge_type
+  end
 end
