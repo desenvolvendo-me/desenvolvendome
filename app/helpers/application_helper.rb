3dbab208fc77e3bbea3db2cc96e5c852815dd16f
Marco Castro <marcodotcastro@gmail.com>
2020-01-19 10:36:26 -0300

    [r] people_same_knowledge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,10 +14,12 @@ module ApplicationHelper
 
   def knowledge_type(language_description)
     knowledge_type = "empty"
-
     return knowledge_type unless language_description
 
-    knowledge_type = Knowledge.joins(:language).where("languages.description = ?", language_description).take.knowledge_type
+    knowledge = Knowledge.joins(:language).where("languages.description = ?", language_description).take
+    return knowledge_type unless knowledge
+
+    knowledge_type = knowledge.knowledge_type
     knowledge_type
   end
 end
