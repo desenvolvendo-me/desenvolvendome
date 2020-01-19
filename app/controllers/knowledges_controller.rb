e94ef25490fad751b13c00d41606d30b8dc0bb2f
Marco Castro <marcodotcastro@gmail.com>
2020-01-19 10:35:55 -0300

    [a] all knowledge controller

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -6,8 +6,8 @@ class KnowledgesController < ApplicationController
   end
 
   def all
-    @q = Knowledge.ransack(params[:q])
-    @knowledges = @q.result(distinct: true).page params[:page]
+    @q = Language.joins(:knowledges).ransack(params[:q])
+    @languages = @q.result.group(:id).order('COUNT(knowledges.id) DESC').page params[:page]
   end
 
 end
