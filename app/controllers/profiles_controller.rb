b4b167becdb8c1ac1480608e8576f4423408d4af
Marco Castro <marcodotcastro@gmail.com>
2020-01-25 13:36:16 -0300

    [r] languages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,8 @@ class ProfilesController < ApplicationController
 
   def index
     @q = Profile.joins(:user).ransack(params[:q])
-    @profiles = @q.result(distinct: true).not_hidden.order(created_at: :desc).page params[:page]
+    @profiles = @q.result.not_hidden.order(created_at: :desc).page params[:page]
+    @languages = Language.joins(knowledges: :profile).where("profiles.id in (?)", @profiles.pluck(:id)).order(description: :asc).distinct
   end
 
   def show
