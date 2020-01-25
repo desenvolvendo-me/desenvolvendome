145e2cd341372be615526d6f668a6269b66fe476
Marco Castro <marcodotcastro@gmail.com>
2020-01-25 13:21:43 -0300

    [r] profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,16 +11,4 @@ class ProfilesController < ApplicationController
   def show
   end
 
-  def starteds
-    @profiles = Profile.joins(:evaluation).where("evaluations.evaluation_type = ?", Evaluation.evaluation_types[:started]).order("evaluations.xp": :desc).page params[:page]
-  end
-
-  def novices
-    @profiles = Profile.joins(:evaluation).where("evaluations.evaluation_type = ?", Evaluation.evaluation_types[:novice]).order("evaluations.xp": :desc).page params[:page]
-  end
-
-  def knights
-    @profiles = Profile.joins(:evaluation).where("evaluations.evaluation_type = ?", Evaluation.evaluation_types[:knight]).order("evaluations.xp": :desc).page params[:page]
-  end
-
 end
