bf1b3a8cb2bfb7a4dfb5eed270a64e15f15bef61
Marco Castro <marcodotcastro@gmail.com>
2020-01-26 12:04:18 -0300

    [f] enum

--- app/helpers/enum_i18n_helper.rb
+++ app/helpers/enum_i18n_helper.rb
@@ -2,7 +2,7 @@ module EnumI18nHelper
 
   def enum_options_for_select(class_name, enum)
     class_name.send(enum.to_s.pluralize).map do |key, _|
-      [enum_i18n(class_name, enum, key), key]
+      [class_name.human_enum_name(enum, key), key]
     end
   end
 
