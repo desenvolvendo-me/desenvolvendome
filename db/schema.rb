3420058c390c6a63697b5c20409a5f6ef648198a
Marco Castro <marcodotcastro@gmail.com>
2020-02-03 10:41:09 -0300

    [c] changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_130405) do
+ActiveRecord::Schema.define(version: 2020_02_03_130406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,6 +224,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_130405) do
     t.string "whodunnit"
     t.text "object"
     t.datetime "created_at"
+    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
