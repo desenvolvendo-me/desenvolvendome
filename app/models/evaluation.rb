ccae26cbc2d49c13837e5f8d03ca83312944be95
Marco Castro <marcodotcastro@gmail.com>
2020-02-22 11:55:31 -0300

    [r] xp

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -20,25 +20,12 @@
 #
 
 class Evaluation < ApplicationRecord
+  include ConvertXpToByte
   has_paper_trail only: [:xp, :level, :evaluation_type]
   belongs_to :profile
 
   enum evaluation_type: [:started, :novice, :knight]
 
   validates_presence_of :evaluation_type, :level
-
-  def xp
-    number = 0
-    bytes = self[:xp].to_i
-    kilobytes = (bytes.to_f / 1024)
-    megabytes = (kilobytes.to_f / 1024)
-    gigabytes = (megabytes.to_f / 1024)
-
-    number = bytes.to_f.round(2).to_s + "b" if bytes > 1
-    number = kilobytes.to_f.round(2).to_s + "kb" if kilobytes > 1
-    number = megabytes.to_f.round(2).to_s + "mb" if megabytes > 1
-    number = gigabytes.to_f.round(2).to_s + "gb" if gigabytes > 1
-    number
-  end
 
 end
