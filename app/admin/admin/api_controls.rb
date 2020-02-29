4c3aba4f144cc18912100ad832d5d7e79f55ce0b
Marco Castro <marcodotcastro@gmail.com>
2020-02-29 10:59:17 -0300

    [a] admin

---
+++ app/admin/admin/api_controls.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register ApiControl do
+  menu priority: 4
+  menu parent: "Administração"
+
+  actions :index, :show, :edit
+
+  index do
+    selectable_column
+    id_column
+    column :key
+    column :limit
+    column :consume
+    column :reset
+    actions
+  end
+
+  filter :key
+
+
+end
