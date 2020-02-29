765917b3101a2f44a36ef14e731dc858720bb0cb
Marco Castro <marcodotcastro@gmail.com>
2020-02-29 15:45:26 -0300

    [r] alterado importação de repositorios

--- app/businesses/github/importation/repo.rb
+++ app/businesses/github/importation/repo.rb
@@ -11,21 +11,17 @@ class Github::Importation::Repo < Github::Importation
   private
 
   def import
-    pages, repositories_count = calc_pages
-    pages.times do |page|
-      get_repos(page, repositories_count)
-    end
+    get_repos(@user.repositories_count)
   end
 
-  def get_repos(page, repositories_count)
-    @github.repos(@user.login, page).each.with_index do |repo, index|
+  def get_repos(repositories_count)
+    @github.repos(@user.login, repositories_count).each.with_index do |repo, index|
       Rails.logger.info "User: #{@user.login}, Repository: #{repo['name']}, #{index + 1} to #{repositories_count}"
       set_repository(repo)
     end
   end
 
   def set_repository(repo)
-    repo = get_repo(repo)
     repository = Repository.find_by(github_id: repo['id'])
     if repository
       repository.update(github_id: repo['id'],
@@ -47,16 +43,6 @@ class Github::Importation::Repo < Github::Importation
                         pushed_at: repo['pushed_at'].to_date,
                         size: repo['size'], user: @user)
     end
-  end
-
-  def calc_pages
-    repositories_count = @user.repositories_count
-    pages = (repositories_count / 100) + 1
-    return pages, repositories_count
-  end
-
-  def get_repo(repo)
-    @github.repo(@user.login, repo['name'])
   end
 
 end
