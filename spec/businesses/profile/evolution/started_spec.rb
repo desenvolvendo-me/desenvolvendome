b964a2e568beff50f02700e00adab1b0302de436
Marco Castro <marcodotcastro@gmail.com>
2020-05-02 17:16:35 -0300

    [a] alterado test calculo de level

--- spec/businesses/profile/evolution/started_spec.rb
+++ spec/businesses/profile/evolution/started_spec.rb
@@ -11,34 +11,46 @@ RSpec.describe 'Profile::Evolution::Started' do
   context 'level' do
 
     it '1' do
+      create(:contribution, additions: 100, deletions: 20, commits: 2, period: '01/01/2020'.to_datetime, contributor: @contributor)
+
       Profile::Evolution::Started.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(17)
       expect(@user.profile.evaluation.level).to eq(1)
     end
 
     it '2' do
-      create(:contribution, additions: 13, deletions: 2, commits: 1, contributor: @contributor)
+      create(:contribution, additions: 100, deletions: 20, commits: 3, period: '01/01/2020'.to_datetime, contributor: @contributor)
 
       Profile::Evolution::Started.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
-      expect(@user.profile.evaluation.xp).to eq(5)
+      expect(@user.profile.evaluation.xp).to eq(25)
       expect(@user.profile.evaluation.level).to eq(2)
     end
 
-    it '10' do
-      create(:repository, commits_count: 15, user: @user)
-      create(:repository, commits_count: 15, user: @user)
-      create(:repository, commits_count: 30, user: @user)
-      create(:repository, commits_count: 30, user: @user)
-      create(:repository, commits_count: 11, user: @user)
-      create(:repository, commits_count: 40, user: @user)
-      create(:repository, commits_count: 8, user: @user)
+    it '8' do
+      10.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "#{n+1}/01/2020".to_datetime, contributor: @contributor)
+      end
 
       Profile::Evolution::Started.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(779)
+      expect(@user.profile.evaluation.level).to eq(8)
+    end
+
+    it '10' do
+      10.times do |n|
+        create(:contribution, additions: 1200, deletions: 200, commits: 193, period: "#{n+1}/01/2020".to_datetime, contributor: @contributor)
+      end
+
+      Profile::Evolution::Started.new(@user).run
+
+      expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(1379)
       expect(@user.profile.evaluation.level).to eq(10)
     end
 
