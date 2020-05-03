8f8b2d9c590939c7369fc4d4e52464334844a2c1
Marco Castro <marcodotcastro@gmail.com>
2020-05-03 14:03:24 -0300

    [f] evolution knight test

--- spec/businesses/profile/evolution_spec.rb
+++ spec/businesses/profile/evolution_spec.rb
@@ -6,66 +6,85 @@ RSpec.describe "Profile::Evolution" do
     @user = create(:user, :with_profile)
     repository = create(:repository, commits_count: 13, user: @user)
     @contributor = create(:contributor, login: @user.login, repository: repository)
+
+    @date_start = "01/01/2020".to_date
+    @week = 0
   end
 
   context "level" do
 
     it 'started' do
       1.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
+
+      # Started
 
       Profile::Evolution.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(78)
       expect(@user.profile.evaluation.level).to eq(3)
 
       repository = create(:repository, commits_count: 40, user: @user)
       @contributor = create(:contributor, login: @user.login, repository: repository)
       1.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
 
       repository = create(:repository, commits_count: 40, user: @user)
       @contributor = create(:contributor, login: @user.login, repository: repository)
       2.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
+
+      # Started
 
       Profile::Evolution.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
-      expect(@user.profile.evaluation.level).to eq(4)
+      expect(@user.profile.evaluation.xp).to eq(390)
+      expect(@user.profile.evaluation.level).to eq(6)
     end
 
     it 'novice' do
-      date_start = "01/01/2020".to_date
+      # Started
 
       1.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: date_start + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
 
       Profile::Evolution.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(78)
       expect(@user.profile.evaluation.level).to eq(3)
+
+      # Novice
 
       repository = create(:repository, commits_count: 50, user: @user)
       @contributor = create(:contributor, login: @user.login, repository: repository)
       10.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: date_start + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
 
       repository = create(:repository, commits_count: 50, user: @user)
       @contributor = create(:contributor, login: @user.login, repository: repository)
       8.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: date_start + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
 
       repository = create(:repository, commits_count: 50, user: @user)
       @contributor = create(:contributor, login: @user.login, repository: repository)
       1.times do |n|
-        create(:contribution, additions: 325, deletions: 60, commits: 30, period: date_start + (n + 1).week, contributor: @contributor)
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
       end
 
       Profile::Evolution.new(@user).run
@@ -76,34 +95,75 @@ RSpec.describe "Profile::Evolution" do
     end
 
     it 'knight' do
-      create(:repository, commits_count: 14, user: @user)
+      1.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
+
+      # Started
+      Profile::Evolution.new(@user).run
+
+      expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(78)
+      expect(@user.profile.evaluation.level).to eq(3)
+
+      # Novice
+
+      repository = create(:repository, commits_count: 50, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      10.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
+
+      repository = create(:repository, commits_count: 50, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      8.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
+
+      repository = create(:repository, commits_count: 50, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      1.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
 
       Profile::Evolution.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("started")
-      expect(@user.profile.evaluation.level).to eq(1)
+      expect(@user.profile.evaluation.novice?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(1558)
+      expect(@user.profile.evaluation.level).to eq(3)
 
-      create(:repository, commits_count: 50, user: @user)
-      create(:repository, commits_count: 50, user: @user)
-      create(:repository, commits_count: 50, user: @user)
-      create(:repository, commits_count: 15, user: @user)
+      # Knight
+
+      repository = create(:repository, commits_count: 100, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      17.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
+
+      repository = create(:repository, commits_count: 100, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      7.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
+
+      repository = create(:repository, commits_count: 100, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      10.times do |n|
+        @week += 1
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: @date_start + (@week).week, contributor: @contributor)
+      end
 
       Profile::Evolution.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("novice")
-      expect(@user.profile.evaluation.level).to eq(1)
-      expect(@user.repositories.sum(:commits_count)).to eq(179)
-
-      create(:repository, commits_count: 100, user: @user)
-      create(:repository, commits_count: 100, user: @user)
-      create(:repository, commits_count: 100, user: @user)
-      create(:repository, commits_count: 177, user: @user)
-
-      Profile::Evolution.new(@user).run
-
-      expect(@user.profile.evaluation.evaluation_type).to eq("knight")
-      expect(@user.profile.evaluation.level).to eq(2)
-      expect(@user.repositories.sum(:commits_count)).to eq(656)
+      expect(@user.profile.evaluation.knight?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(4208)
+      expect(@user.profile.evaluation.level).to eq(5)
     end
 
   end
