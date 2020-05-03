6d4ae3f05524bae741370a6769704b7c826e5276
Marco Castro <marcodotcastro@gmail.com>
2020-05-03 12:28:17 -0300

    [f] test nivel started

--- spec/businesses/profile/evolution/started_spec.rb
+++ spec/businesses/profile/evolution/started_spec.rb
@@ -43,14 +43,14 @@ RSpec.describe 'Profile::Evolution::Started' do
     end
 
     it '10' do
-      10.times do |n|
-        create(:contribution, additions: 1200, deletions: 200, commits: 193, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      18.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
       end
 
       Profile::Evolution::Started.new(@user).run
 
       expect(@user.profile.evaluation.started?).to be_truthy
-      expect(@user.profile.evaluation.xp).to eq(1379)
+      expect(@user.profile.evaluation.xp).to eq(1403)
       expect(@user.profile.evaluation.level).to eq(10)
     end
 
