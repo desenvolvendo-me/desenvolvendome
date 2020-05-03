aa6f91937862d1353d40a4ea7801188bfe14f16e
Marco Castro <marcodotcastro@gmail.com>
2020-05-03 12:29:01 -0300

    [a] test evolution started

--- spec/businesses/profile/evolution_spec.rb
+++ spec/businesses/profile/evolution_spec.rb
@@ -4,26 +4,38 @@ RSpec.describe "Profile::Evolution" do
 
   before(:each) do
     @user = create(:user, :with_profile)
+    repository = create(:repository, commits_count: 13, user: @user)
+    @contributor = create(:contributor, login: @user.login, repository: repository)
   end
 
   context "level" do
 
     it 'started' do
-      create(:repository, commits_count: 14, user: @user)
+      1.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
 
       Profile::Evolution.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("started")
-      expect(@user.profile.evaluation.level).to eq(1)
+      expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.level).to eq(3)
 
-      create(:repository, commits_count: 40, user: @user)
-      create(:repository, commits_count: 40, user: @user)
+      repository = create(:repository, commits_count: 40, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      1.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
+
+      repository = create(:repository, commits_count: 40, user: @user)
+      @contributor = create(:contributor, login: @user.login, repository: repository)
+      2.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
 
       Profile::Evolution.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("started")
-      expect(@user.profile.evaluation.level).to eq(7)
-      expect(@user.repositories.sum(:commits_count)).to eq(94)
+      expect(@user.profile.evaluation.started?).to be_truthy
+      expect(@user.profile.evaluation.level).to eq(4)
     end
 
     it 'novice' do
@@ -75,114 +87,6 @@ RSpec.describe "Profile::Evolution" do
       expect(@user.profile.evaluation.evaluation_type).to eq("knight")
       expect(@user.profile.evaluation.level).to eq(2)
       expect(@user.repositories.sum(:commits_count)).to eq(656)
-    end
-
-    it 'marcodotcastro' do
-      create(:repository, commits_count: 48, user: @user)
-      create(:repository, commits_count: 68, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 4, user: @user)
-      create(:repository, commits_count: 6, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 39, user: @user)
-      create(:repository, commits_count: 88, user: @user)
-      create(:repository, commits_count: 61, user: @user)
-      create(:repository, commits_count: 212, user: @user)
-      create(:repository, commits_count: 2, user: @user)
-      create(:repository, commits_count: 3, user: @user)
-      create(:repository, commits_count: 86, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 39, user: @user)
-
-      Profile::Evolution.new(@user).run
-
-      expect(@user.profile.evaluation.evaluation_type).to eq("novice")
-      expect(@user.profile.evaluation.level).to eq(10)
-      expect(@user.repositories.sum(:commits_count)).to eq(656)
-    end
-
-    it 'MoacirPetry' do
-      create(:repository, commits_count: 48, user: @user)
-      create(:repository, commits_count: 5, user: @user)
-      create(:repository, commits_count: 25, user: @user)
-      create(:repository, commits_count: 4, user: @user)
-      create(:repository, commits_count: 4, user: @user)
-      create(:repository, commits_count: 15, user: @user)
-      create(:repository, commits_count: 5, user: @user)
-      create(:repository, commits_count: 29, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 26, user: @user)
-      create(:repository, commits_count: 124, user: @user)
-
-      Profile::Evolution.new(@user).run
-
-      expect(@user.profile.evaluation.evaluation_type).to eq("started")
-      expect(@user.profile.evaluation.level).to eq(10)
-      expect(@user.repositories.sum(:commits_count)).to eq(285)
-    end
-
-    it 'leonnleite' do
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 21, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 2, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 2, user: @user)
-      create(:repository, commits_count: 6, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 6, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 2, user: @user)
-      create(:repository, commits_count: 3, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 15, user: @user)
-      create(:repository, commits_count: 4, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 2, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 5, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 3, user: @user)
-      create(:repository, commits_count: 9, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 3, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 2, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 0, user: @user)
-      create(:repository, commits_count: 1, user: @user)
-
-      Profile::Evolution.new(@user).run
-
-      expect(@user.profile.evaluation.evaluation_type).to eq("started")
-      expect(@user.profile.evaluation.level).to eq(7)
-      expect(@user.repositories.sum(:commits_count)).to eq(94)
     end
 
   end
