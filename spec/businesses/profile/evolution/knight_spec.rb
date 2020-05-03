e42b7648054ad51f62838c7f4fe25d245cfb5b88
Marco Castro <marcodotcastro@gmail.com>
2020-05-03 12:18:25 -0300

    [a] test nivel knight

--- spec/businesses/profile/evolution/knight_spec.rb
+++ spec/businesses/profile/evolution/knight_spec.rb
@@ -4,41 +4,57 @@ RSpec.describe "Profile::Evolution::Knight" do
 
   before(:each) do
     @user = create(:user, :with_profile)
-
-    create(:repository, commits_count: 100, user: @user)
-    create(:repository, commits_count: 100, user: @user)
-    create(:repository, commits_count: 100, user: @user)
+    repository = create(:repository, commits_count: 13, user: @user)
+    @contributor = create(:contributor, login: @user.login, repository: repository)
   end
 
   context "level" do
 
     it "1" do
+      36.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
+
       Profile::Evolution::Knight.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("knight")
+      expect(@user.profile.evaluation.knight?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(2805)
       expect(@user.profile.evaluation.level).to eq(1)
     end
 
     it "2" do
-      create(:repository, commits_count: 100, user: @user)
-      create(:repository, commits_count: 200, user: @user)
+      37.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
 
       Profile::Evolution::Knight.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("knight")
+      expect(@user.profile.evaluation.knight?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(2883)
       expect(@user.profile.evaluation.level).to eq(2)
     end
 
-    it '10' do
-      create(:repository, commits_count: 1000, user: @user)
-      create(:repository, commits_count: 500, user: @user)
-      create(:repository, commits_count: 500, user: @user)
-      create(:repository, commits_count: 500, user: @user)
-      create(:repository, commits_count: 200, user: @user)
+    it "8" do
+      80.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
 
       Profile::Evolution::Knight.new(@user).run
 
-      expect(@user.profile.evaluation.evaluation_type).to eq("knight")
+      expect(@user.profile.evaluation.knight?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(6234)
+      expect(@user.profile.evaluation.level).to eq(8)
+    end
+
+    it "10" do
+      95.times do |n|
+        create(:contribution, additions: 325, deletions: 60, commits: 30, period: "01/01/2020".to_date + (n+1).week, contributor: @contributor)
+      end
+
+      Profile::Evolution::Knight.new(@user).run
+
+      expect(@user.profile.evaluation.knight?).to be_truthy
+      expect(@user.profile.evaluation.xp).to eq(7403)
       expect(@user.profile.evaluation.level).to eq(10)
     end
 
