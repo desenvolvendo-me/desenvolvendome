b370417e90c2f45c881cdcf660037f41c21fb9d6
Marco Castro <marcodotcastro@gmail.com>
2020-05-04 19:25:14 -0300

    feat(importação): alterado reimportação
    movido a reimportação do perfil do usuário para a página de repositórios

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,12 +19,8 @@ class ProfilesController < ApplicationController
   end
 
   def reimport
-    if current_user.can_evaluation?
-      GenerateProfileJob.perform_later(current_user.login)
-      redirect_to profile_show_path(current_user.login), notice: 'Reimportação Envia com Sucesso'
-    else
-      redirect_to reimport_rule_path
-    end
+    GenerateProfileJob.perform_later(current_user.login)
+    redirect_to repositories_user_path(current_user.login), notice: 'Reimportação Envia com Sucesso'
   end
 
   def reimport_rule;
