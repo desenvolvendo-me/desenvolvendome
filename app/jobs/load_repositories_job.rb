b0966b81f26a22f9838e1e53683e996ea147f0d5
Marco Castro <marcodotcastro@gmail.com>
2020-05-09 17:44:44 -0300

    fix(histórico de evolução): profile generate duplicado

--- app/jobs/load_repositories_job.rb
+++ app/jobs/load_repositories_job.rb
@@ -5,23 +5,12 @@ class LoadRepositoriesJob < ApplicationJob
     user = User.find_by_login(login)
 
     import_github(user)
-    profile_generate(user)
-    #NOTE: Não será enviado email ainda
-    # send_profile(user)
     evaluations_count(user)
   end
 
   def import_github(user)
     Github::Importation.new.author(user)
     Github::Importation.new.reposities(user)
-  end
-
-  def profile_generate(user)
-    Profile::Generate.new(user).run
-  end
-
-  def send_profile(user)
-    ProfileMailer.with(user: user).evaluation_completed.deliver_now
   end
 
   def evaluations_count(user)
