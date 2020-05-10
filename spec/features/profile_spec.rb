929729f9e6613a50217385911ac76c9291ab5448
Marco Castro <marcodotcastro@gmail.com>
2020-05-10 14:53:44 -0300

    test(qualidade): adicionado teste de feature

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -13,18 +13,4 @@ RSpec.feature "Profile", :type => :feature do
     expect(page).to have_text("seus repositórios")
   end
 
-  scenario "user ocultar perfil" do
-    user = create(:user, :with_profile)
-    login_as(user, :scope => :user)
-
-    visit "/"
-
-    click_on "Meu Perfil"
-
-    expect(page).to have_text("Perfil Visível")
-
-    click_on "Perfil Visível"
-
-    expect(page).to have_text("Perfil Oculto")
-  end
 end
